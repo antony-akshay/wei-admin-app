0af6f0eda88455e275164abf535ee6bbf4ce3754
antony-akshay <akshay.dev.ac.in@gmail.com>
2025-07-05 16:07:47 +0530

    forgot_password_screen

--- lib/routes/app_route_constants.dart
+++ lib/routes/app_route_constants.dart
@@ -5,4 +5,5 @@ class AppRouteNames {
   static const String adminSignup = "admin_signup";
   static const String organizationSignup = "organization_signup";
   static const String otpVerification = "otp_verification";
+  static const String forgotPassword = "forgot_password";
 }
